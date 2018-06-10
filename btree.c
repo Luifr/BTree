e95d3e3c5b5ed868947c0a771b76cc2cfd20155b
luifr <luifr@usp.br>
2018-06-10 16:29:26 -0300

    btree files

---
+++ btree.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+void insertBTree(){
+
+}
+
+void searchBTree(){
+
+}
+
+void removeBTree(){
+
+}
---
+++ btree.h
@@ -0,0 +1,10 @@
+#ifndef BTREE_H
+#define BTREE_H
+
+void insertBTree();
+
+void searchBTree();
+
+void deleteBTree();
+
+#endif
